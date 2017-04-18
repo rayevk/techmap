f730dc00453739d61e3271acd5e23ed260b5a116
André Ruffert <andre@andreruffert.com>
2017-04-18 17:03:13 +0200

    chore: introduce script to create json data from csv file

---
+++ scripts/createData.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+const csvParser = require('csvtojson');
+const stream = require('stream');
+const kebabCase = require('lodash.kebabcase');
+
+const csvReadStream = fs.createReadStream(path.join(process.cwd(), '__csv__/london.csv'));
+const jsonWriteStream = fs.createWriteStream(path.join(process.cwd(), 'public/data/london.json'));
+
+csvParser()
+  .fromStream(csvReadStream)
+  .on('done', err => {
+    jsonWriteStream.write(']');
+    console.log('end');
+  })
+  .pipe(transform())
+  .pipe(jsonWriteStream);
+
+
+function transform() {
+  let rowIndex = 0;
+  return new stream.Transform({
+    objectMode: true,
+    transform(chunk, encoding, cb) {
+      const jsonObj = JSON.parse(chunk.toString());
+      const dataStr = JSON.stringify({
+        id: kebabCase(jsonObj['Company']),
+        idx: rowIndex + 1,
+        name: jsonObj['Company'],
+        industry: jsonObj['Industry'],
+        station: jsonObj['Tube'],
+        adress: jsonObj['Address'],
+        techStack: [
+          jsonObj['Frontend Language'],
+          jsonObj['Backend Language'],
+          jsonObj['Frontend Frameworks'],
+          jsonObj['Backend Frameworks'],
+          jsonObj['Backend testing frameworks'],
+          jsonObj['Frontend testing frameworks'],
+          jsonObj['Bundlers'],
+          jsonObj['Supersets']
+        ].filter(techStack => techStack)
+         .filter(techStack => techStack !== '-')
+         .reduce((a, b) => a.concat(b.split(', ')), [])
+      }, null, 2);
+
+      this.push((rowIndex) ? `,\n${dataStr}` : `[${dataStr}`);
+      rowIndex++;
+      cb();
+    }
+  });
+}
