ebf9264002ab6a72d4f60601a01b3efa7538ac07
André Ruffert <andre@andreruffert.com>
2017-05-03 17:38:36 +0200

    fix: remove unused/undefined `settings.toolbar.height`

--- src/components/Viewport/index.js
+++ src/components/Viewport/index.js
@@ -3,7 +3,6 @@ import settings from '../../styles/settings';
 
 const Viewport = styled.div`
   background-color: #fbfbfb;
-  // height: calc(100% - ${settings.toolbar.height});
   height: 100%;
   position: relative;
   overflow: hidden;
