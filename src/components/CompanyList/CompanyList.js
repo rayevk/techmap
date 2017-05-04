54fb797d2a924f6350892cf55c5a4a99eafa762a
André Ruffert <andre@andreruffert.com>
2017-05-04 17:24:37 +0200

    refactor: use stateless functional component

--- src/components/CompanyList/CompanyList.js
+++ src/components/CompanyList/CompanyList.js
@@ -1,38 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import PaneHeader from '../PaneHeader';
 import Item from './Item';
 import { applyFilters } from '../../utils';
 
-class CompanyList extends Component {
-  render() {
-    const { companies } = this.props.globalStore;
-    const filters = this.props.params;
-    const paneTitle = Object.values(filters)[0];
-    const companyItems = companies
-      .filter(company => applyFilters(company, filters))
-      .map((company, idx) => (
-        <Item
-          to={`/london/company/${encodeURIComponent(company.name)}`}
-          key={idx}
-        >
-          {company.name}
-        </Item>
-      ));
+const CompanyList = props => {
+  const { companies } = props.globalStore;
+  const filters = props.params;
+  const paneTitle = Object.values(filters)[0];
+  const companyItems = companies
+    .filter(company => applyFilters(company, filters))
+    .map((company, idx) => (
+      <Item
+        to={`/london/company/${encodeURIComponent(company.name)}`}
+        key={idx}
+      >
+        {company.name}
+      </Item>
+    ));
 
-    return (
-      <div>
-        <PaneHeader
-          title={paneTitle}
-          meta={`${companyItems.length} companies`}
-          backButton="true"
-          backTo="/london"
-        />
-        {companyItems}
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <PaneHeader
+        title={paneTitle}
+        meta={`${companyItems.length} companies`}
+        backButton="true"
+        backTo="/london"
+      />
+      {companyItems}
+    </div>
+  );
+};
 
 CompanyList.propTypes = {
   globalStore: PropTypes.object,
