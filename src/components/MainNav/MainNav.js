adc852dd50d4434aa9f6add8e1f82ef7a47f9b08
André Ruffert <andre@andreruffert.com>
2017-05-16 16:22:27 +0200

    feat: reorder MainNav content

--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -26,6 +26,30 @@ class MainNav extends Component {
           visible={this.state.open}
           onRequestChange={open => this.setState({ open })}
         >
+          <Content>
+            <h3>Honeypot’s London Tech Map</h3>
+            <p>
+              The navigation bar on the left allows you to view companies by industry, location and programming language. For research findings and analysis, check out our
+              {' '}
+              <a
+                href="https://honeypotio.github.io/research/pages/london-techmap.html"
+                target="_blank"
+                rel="noopener"
+              >
+                research report
+              </a>
+              . And remember the tech map is
+              {' '}
+              <a
+                href="https://github.com/honeypotio/techmap"
+                target="_blank"
+                rel="noopener"
+              >
+                open source
+              </a>
+              ! Add or edit your company by creating a pull request.
+            </p>
+          </Content>
           <Item
             href="https://github.com/honeypotio/techmap"
             target="_blank"
@@ -51,31 +75,9 @@ class MainNav extends Component {
             >
               Join Honeypot
             </Button>
-            <p>
-              <a href="https://www.honeypot.io" target="_blank" rel="noopener">
-                Honeypot
-              </a>
-              {' '}
-              is Europe’s developer-focused job platform. We are on a mission to get every developer a great job. To create the London Tech Map, we looked at over 12,000 developer job postings and the tech stacks of over 800 London companies. The navigation bar on the left allows you to view companies by industry, location and programming language. For research findings and analysis, check out our
-              {' '}
-              <a
-                href="https://honeypotio.github.io/research/pages/london-techmap.html"
-                target="_blank"
-                rel="noopener"
-              >
-                research report
-              </a>
-              . And remember the tech map is
-              {' '}
-              <a
-                href="https://github.com/honeypotio/techmap"
-                target="_blank"
-                rel="noopener"
-              >
-                open source
-              </a>
-              ! Add or edit your company by creating a pull request.
-            </p>
+            <small>
+              Honeypot is Europe’s developer-focused job platform. We are on a mission to get every developer a great job. To create the London Tech Map, we looked at over 12,000 developer job postings and the tech stacks of over 800 London companies.
+            </small>
           </Content>
         </Drawer>
       </div>
