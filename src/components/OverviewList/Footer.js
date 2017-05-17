f599d351a8cf76ffc09fd73a9932b936a5b8705e
André Ruffert <andre@andreruffert.com>
2017-05-17 12:54:11 +0200

    fix: OverviewList UI for small screens
    0795674

--- src/components/OverviewList/Footer.js
+++ src/components/OverviewList/Footer.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
+import { media } from '../../styles/utils';
 
 const Footer = styled.div`
   border-top: 1px solid #f2f2f2;
   padding: 15px 20px;
-  margin-top: auto;
+
+  ${media.large`
+    /* push to the bottom */
+    margin-top: auto;
+  `}
 `;
 
 export default Footer;
--- src/components/OverviewList/Wrapper.js
+++ src/components/OverviewList/Wrapper.js
@@ -1,11 +1,14 @@
 import styled from 'styled-components';
+import { media } from '../../styles/utils';
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  ${media.large`
+    display: flex;
+    flex-direction: column;
 
-  /* Exclude pane header height */
-  height: calc(100% - 94px);
+    /* Push <Footer /> to the bottom (exclude pane header height) */
+    height: calc(100% - 94px);
+  `}
 `;
 
 export default Wrapper;
